9c8fef560b62b57e39423d702fb64cb005d419a6
m1k4n <mamlpm95@gmail.com>
2024-11-09 22:18:57 +0100

    definning class

---
+++ mUTcPP.cpp
@@ -0,0 +1,5 @@
+#include "mUTcPP.h"
+
+mUTcPP::mUTcPP(vector<testingFunction> parsedVector){
+    functionsToTest_ = parsedVector;
+}
--- mUTcPP.h
+++ mUTcPP.h
@@ -2,13 +2,33 @@
 #define mUTcPP_h
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
-class mUTcPP 
+class mUTcPP
 {
-    public:
-        typedef bool (*testingFunction)();
-        mUTcPP(int* pointerToFunctionArray);
-    private:
-        vector<testingFunction> functionsToTest_;
+public:
+    typedef bool (*testingFunction)();
+    mUTcPP(vector<testingFunction> parsedVector);
+    enum messageType
+    {
+        message = 0,
+        error = 31,
+        success = 32
+    };
+    //TODO
+    template <typename T>
+    bool checkIfFunctionWorks(testingFunction functionPointer, T expectedValue);
+    //TODO
+    void setRedTerminal();
+    //TODO
+    void setGreendTerminal();
+    //TODO
+    void setDefaultTerminal();
+    //TODO
+    void writeMessage(messageType messageT, string message);
+
+private:
+    vector<testingFunction>
+        functionsToTest_;
 };
 #endif
